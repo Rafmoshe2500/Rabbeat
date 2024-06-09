226674550d252891e5a18cc203e1ea10b411745f
Yair Avivi <yavivi441@gmail.com>
2024-06-09 22:08:40 +0300

    add select component

---
+++ client/src/components/select/auto-width-select.tsx
@@ -0,0 +1,44 @@
+import {
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  SelectChangeEvent,
+} from "@mui/material";
+
+type AutoWidthSelectProps = {
+  label: string;
+  value: string;
+  options: string[];
+  onChange: (event: SelectChangeEvent<string>) => void;
+};
+
+const AutoWidthSelect = ({
+  label,
+  value,
+  options,
+  onChange,
+}: AutoWidthSelectProps) => {
+  return (
+    <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <InputLabel id="demo-simple-select-autowidth-label">{label}</InputLabel>
+      <Select
+        labelId="demo-simple-select-autowidth-label"
+        id="demo-simple-select-autowidth"
+        value={value}
+        onChange={onChange}
+        autoWidth
+        label={label}
+        sx={{ direction: "rtl" }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default AutoWidthSelect;
