3246f9c3fffdb89b07369dba4234dadd11e11c3b
Yair Avivi <yavivi441@gmail.com>
2024-06-18 23:09:04 +0300

    add hook for fetching all lessons

---
+++ client/src/hooks/useAllLessons.tsx
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllLessons } from "../api/endpoints/lesson";
+
+const createQueryKey = (keys: string[]) => [...keys] as const;
+
+export const useAllLessons = () => {
+  const queryKey = createQueryKey(["allLessons"]);
+  return useQuery<Lesson[], Error>({
+    queryKey,
+    queryFn: getAllLessons,
+  });
+};
