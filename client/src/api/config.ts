c1b75de9ba452c044b8cc88261766083de9546d2
yairavivi4 <yavivi441@gmail.com>
2024-06-19 14:15:38 +0300

    fix root api

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://10.10.248.125/',
+  baseURL: 'http://10.10.248.125/api/',
   headers: {
     'Content-Type': 'application/json',
   },
