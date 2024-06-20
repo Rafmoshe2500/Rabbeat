eda16d576b80ed3fec72b5b6f57017d4b5a5025f
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-06-20 14:22:15 +0300

    add chat and vhange humash to hebrew

--- server/app.py
+++ server/app.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
-# from routers.chat import chat_router
+from routers.chat import chat_router
 from routers import lessons, student_lessons, teacher_lesson, lesson_comment, lesson_status, lesson_chatbot, user
 from routers.torah import torah_router
-# from routers.mongo import mongo_router
 
 app = FastAPI(title='Rabbeat')
 
@@ -16,7 +15,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# app.include_router(mongo_router)
 app.include_router(torah_router)
 app.include_router(lessons.router, prefix="/api")
 app.include_router(student_lessons.router, prefix="/api")
@@ -25,6 +23,6 @@ app.include_router(lesson_comment.router, prefix="/api")
 app.include_router(lesson_status.router, prefix="/api")
 app.include_router(lesson_chatbot.router, prefix="/api")
 app.include_router(user.router, prefix="/api")
+app.include_router(chat_router, prefix="/api")
 
-# app.include_router(chat_router)
 uvicorn.run(app, host="0.0.0.0", port=80)
--- server/bot.py
+++ server/bot.py
@@ -1,20 +1,20 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model = AutoModelForCausalLM.from_pretrained("dicta-il/dictalm2.0-instruct")
-tokenizer = AutoTokenizer.from_pretrained("dicta-il/dictalm2.0-instruct")
-
-messages = [
-    {"role": "user", "content": "איזה רוטב אהוב עליך?"},
-    {"role": "assistant",
-     "content": "טוב, אני די מחבב כמה טיפות מיץ לימון סחוט טרי. זה מוסיף בדיוק את הכמות הנכונה של טעם חמצמץ לכל מה שאני מבשל במטבח!"},
-    {"role": "user", "content": "האם יש לך מתכונים למיונז?"}
-]
-
-encoded = tokenizer.apply_chat_template(messages, return_tensors="pt")
-
-generated_ids = model.generate(encoded, max_new_tokens=50, do_sample=True)
-decoded = tokenizer.batch_decode(generated_ids)
-print(decoded[0])
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+#
+# model = AutoModelForCausalLM.from_pretrained("dicta-il/dictalm2.0-instruct")
+# tokenizer = AutoTokenizer.from_pretrained("dicta-il/dictalm2.0-instruct")
+#
+# messages = [
+#     {"role": "user", "content": "איזה רוטב אהוב עליך?"},
+#     {"role": "assistant",
+#      "content": "טוב, אני די מחבב כמה טיפות מיץ לימון סחוט טרי. זה מוסיף בדיוק את הכמות הנכונה של טעם חמצמץ לכל מה שאני מבשל במטבח!"},
+#     {"role": "user", "content": "האם יש לך מתכונים למיונז?"}
+# ]
+#
+# encoded = tokenizer.apply_chat_template(messages, return_tensors="pt")
+#
+# generated_ids = model.generate(encoded, max_new_tokens=50, do_sample=True)
+# decoded = tokenizer.batch_decode(generated_ids)
+# print(decoded[0])
 
 # from transformers import AutoTokenizer, AutoModelForCausalLM
 #
@@ -28,18 +28,18 @@ print(decoded[0])
 # print(tokenizer.decode(outputs[0]))
 
 
-# import vertexai
-# from vertexai.generative_models import GenerativeModel
-# import os
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
-#
-# # TODO(developer): Update and un-comment below line
-# project_id = "graphical-envoy-425419-a8"
-#
-# vertexai.init(project=project_id, location="us-central1")
-#
-# model = GenerativeModel(model_name="gemini-1.5-flash-001")
-#
-#
-# def get_answer(question, parasha):
-#     return model.generate_content([f' דבר איתי בנושא: {parasha} ', question])
+import vertexai
+from vertexai.generative_models import GenerativeModel
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+# TODO(developer): Update and un-comment below line
+project_id = "graphical-envoy-425419-a8"
+
+vertexai.init(project=project_id, location="us-central1")
+
+model = GenerativeModel(model_name="gemini-1.5-flash-001")
+
+
+def get_answer(question, topic):
+    return model.generate_content([f'על הנושא {topic} יש לי שאלה {question} אשמח לתשובה לא ארוכה!'])
