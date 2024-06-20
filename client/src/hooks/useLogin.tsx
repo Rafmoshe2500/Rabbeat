12706be1fb55b486ba7788a6c9465308a0007bbc
Yair Avivi <yavivi441@gmail.com>
2024-06-20 10:51:14 +0300

    add react query for user communication

---
+++ client/src/hooks/useLogin.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../api/endpoints/user";
+
+const createQueryKey = (queryName: string, userId: string) => {
+  return [queryName, userId] as const;
+};
+
+type QueryKey = ReturnType<typeof createQueryKey>;
+
+// Fetch user
+export const useLessonsByUser = (userId: string) => {
+  const queryKey = createQueryKey("user", userId);
+
+  return useQuery<User, Error, any, QueryKey>({
+    queryKey,
+    queryFn: (context) => getUser(context.queryKey[1]),
+  });
+};
