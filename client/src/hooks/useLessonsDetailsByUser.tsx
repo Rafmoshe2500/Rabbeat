f3fa11de139b48a532d391d21400a6a308f14bb5
Yair Avivi <yavivi441@gmail.com>
2024-06-24 10:47:33 +0300

    change file name

---
+++ client/src/hooks/useLessonsDetailsByUser.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { getLessonsDetailsByUser } from "../api/endpoints/lesson";
+
+const createQueryKey = (queryName: string, userId: string) => {
+  return [queryName, userId] as const;
+};
+
+type QueryKey = ReturnType<typeof createQueryKey>;
+
+// Fetch lessons by user
+export const useLessonsDetailsByUser = (userId: string) => {
+  const queryKey = createQueryKey("lessonsDetails", userId);
+
+  return useQuery<LessonDetails[], Error, LessonDetails[], QueryKey>({
+    queryKey,
+    queryFn: (context) => getLessonsDetailsByUser(context.queryKey[1]),
+  });
+};
