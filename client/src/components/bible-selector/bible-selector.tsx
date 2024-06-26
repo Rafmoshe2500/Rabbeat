a69b687c46a627cdc4ec0d897e9a312601923fe4
Yair Avivi <yavivi441@gmail.com>
2024-06-26 13:14:14 +0300

    naming, get set function and update it with the selection

--- client/src/components/bible-selector/bible-selector.tsx
+++ client/src/components/bible-selector/bible-selector.tsx
@@ -1,19 +1,21 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { Bible } from "../../constants/bible.constants";
 import AutoWidthSelect from "../select/auto-width-select";
-import styles from "./bible-selector.module.scss";
+import styles from "./bible-selector.module.css";
 import { useTorahSection } from "../../hooks/useTorahSection";
 import Chapter from "../bible-displayer/chapter";
 import gematriya from "gematriya";
-import { values } from "regenerator-runtime";
-import { findWord } from "../../utils/utils";
+
+type BibleSelectorProps = {
+  setTorahSection: any;
+};
 
 type State = {
-  book: string;
-  fromChapter: string;
-  fromVerse: string;
-  toChapter: string;
-  toVerse: string;
+  pentateuch: string;
+  startChapter: string;
+  startVerse: string;
+  endChapter: string;
+  endVerse: string;
 };
 
 type Action =
@@ -24,11 +26,11 @@ type Action =
   | { type: "SET_TO_VERSE"; payload: string };
 
 const initialState: State = {
-  book: "",
-  fromChapter: "",
-  fromVerse: "",
-  toChapter: "",
-  toVerse: "",
+  pentateuch: "",
+  startChapter: "",
+  startVerse: "",
+  endChapter: "",
+  endVerse: "",
 };
 
 const reducer = (state: State, action: Action): State => {
@@ -36,87 +38,78 @@ const reducer = (state: State, action: Action): State => {
     case "SET_BOOK":
       return {
         ...state,
-        book: action.payload,
-        fromChapter: "",
-        fromVerse: "",
-        toChapter: "",
-        toVerse: "",
+        pentateuch: action.payload,
+        startChapter: "",
+        startVerse: "",
+        endChapter: "",
+        endVerse: "",
       };
     case "SET_FROM_CHAPTER":
       return {
         ...state,
-        fromChapter: action.payload,
-        fromVerse: "",
-        toChapter: "",
-        toVerse: "",
+        startChapter: action.payload,
+        startVerse: "",
+        endChapter: "",
+        endVerse: "",
       };
     case "SET_FROM_VERSE":
       return {
         ...state,
-        fromVerse: action.payload,
-        toChapter: "",
-        toVerse: "",
+        startVerse: action.payload,
+        endChapter: "",
+        endVerse: "",
       };
     case "SET_TO_CHAPTER":
-      return { ...state, toChapter: action.payload, toVerse: "" };
+      return { ...state, endChapter: action.payload, endVerse: "" };
     case "SET_TO_VERSE":
-      return { ...state, toVerse: action.payload };
+      return { ...state, endVerse: action.payload };
     default:
       return state;
   }
 };
 
-const BibleSelector = () => {
+const BibleSelector = ({ setTorahSection }: BibleSelectorProps) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const { book, fromChapter, fromVerse, toChapter, toVerse } = state;
+  const { pentateuch, startChapter, startVerse, endChapter, endVerse } = state;
 
-  const selectedBook = Bible.find((b) => b.name === book);
+  useEffect(() => {
+    setTorahSection({
+      ...state,
+    });
+  }, [state.endVerse]);
+
+  const selectedBook = Bible.find((b) => b.name === pentateuch);
   const chapters = selectedBook ? selectedBook.chapters : [];
   const fromChapterObject =
-    chapters[gematriya(fromChapter, { order: true }) - 1];
+    chapters[gematriya(startChapter, { order: true }) - 1];
   const fromVerses = fromChapterObject ? fromChapterObject.verses : [];
-  const toChapterObject = chapters[gematriya(toChapter, { order: true }) - 1];
+  const toChapterObject = chapters[gematriya(endChapter, { order: true }) - 1];
   const toVerses = toChapterObject ? toChapterObject.verses : [];
 
   const { data, isLoading, error } = useTorahSection(
-    book,
-    fromChapter,
-    fromVerse,
-    toChapter,
-    toVerse
+    pentateuch,
+    startChapter,
+    startVerse,
+    endChapter,
+    endVerse
   );
-  const a = data && findWord(data.nikud, 2);
-  const b = a && data.nikud[a.chapter][a.verse];
-  const c = b?.split(" ") || [];
-  console.log(a);
-  console.log(b);
-  console.log(c[a?.word || 0]);
-
-  // const allWords = Object.values(data || {}).flatMap((chapter) => {
-  //   const versesContent = Object.values(chapter);
-  //   return versesContent.flatMap(
-  //     (verseContent) => verseContent.split(" ") || []
-  //   );
-  // });
-
-  // console.log(allWords);
 
   return (
     <div className={styles["selectors-container"]}>
       <AutoWidthSelect
         label="ספר"
-        value={book}
+        value={pentateuch}
         options={Bible.map((b) => b.name)}
         onChange={(e) =>
           dispatch({ type: "SET_BOOK", payload: e.target.value })
         }
       />
 
-      {book && (
+      {pentateuch && (
         <AutoWidthSelect
           label="מפרק"
-          value={fromChapter}
+          value={startChapter}
           options={chapters.map((_, index) =>
             gematriya(index + 1, { geresh: false, punctuate: false })
           )}
@@ -129,10 +122,10 @@ const BibleSelector = () => {
         />
       )}
 
-      {fromChapter && (
+      {startChapter && (
         <AutoWidthSelect
           label="פסוק"
-          value={fromVerse}
+          value={startVerse}
           options={fromVerses}
           onChange={(e) =>
             dispatch({
@@ -143,10 +136,10 @@ const BibleSelector = () => {
         />
       )}
 
-      {fromVerse && (
+      {startVerse && (
         <AutoWidthSelect
           label="עד פרק"
-          value={toChapter}
+          value={endChapter}
           options={chapters.map((_, index) =>
             gematriya(index + 1, { geresh: false, punctuate: false })
           )}
@@ -159,10 +152,10 @@ const BibleSelector = () => {
         />
       )}
 
-      {toChapter && (
+      {endChapter && (
         <AutoWidthSelect
           label="פסוק"
-          value={toVerse}
+          value={endVerse}
           options={toVerses}
           onChange={(e) =>
             dispatch({
@@ -176,16 +169,19 @@ const BibleSelector = () => {
       <div>
         <h3>מוצג</h3>
         <p>
-          ספר: {book}, מ: {fromChapter}:{fromVerse}, ועד: {toChapter}:{toVerse}
+          ספר: {pentateuch}, מ: {startChapter}:{startVerse}, ועד: {endChapter}:
+          {endVerse}
         </p>
       </div>
 
       <div>
         {isLoading && <p>Loading...</p>}
         {error && <p>Error loading data</p>}
+
         {data && (
           <div>
-            {Object.entries(data.none).map(([chapterKey, chapter]) => (
+            {/* TODO: extract to another componentttttttttttttttttttttttttttttttttttttttttttttttttttt */}
+            {Object.entries(data.both).map(([chapterKey, chapter]) => (
               <Chapter
                 key={chapterKey}
                 chapterKey={chapterKey}
