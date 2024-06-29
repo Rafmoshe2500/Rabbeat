c00ef7e5009b51745a58be5c646d8a2df346bea6
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-06-30 00:27:35 +0300

    update userContext before move to home page

--- client/src/pages/auth.tsx
+++ client/src/pages/auth.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Add this import
+import { useNavigate } from 'react-router-dom';
 import { Paper, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import LoginForm from '../components/auth/login-form';
 import RegisterForm from '../components/auth/register-form';
-
+import { useUser } from '../contexts/user-context'; // Import UserContext
 
 const RotatingPaper = styled(Paper)<{ isflipped: boolean }>(({ isflipped }) => ({
   width: 800,
@@ -65,13 +65,15 @@ const AuthForm: React.FC = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [message, setMessage] = useState<{ text: string; isError: boolean } | null>(null);
   const navigate = useNavigate();
+  const { setUserDetails } = useUser(); 
 
   const toggleForm = () => {
     setIsLogin(!isLogin);
   };
 
-  const handleSuccess = (user: LoginUser) => {
+  const handleSuccess = (user: User) => {
     setMessage({ text: isLogin ? 'התחברת בהצלחה!' : 'נרשמת בהצלחה!', isError: false });
+    setUserDetails(user); 
     setTimeout(() => {
       setMessage(null);
       navigate('/home', { state: { user } });
