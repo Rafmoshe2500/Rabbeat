0e642269ba8cf0364d5b9f87184c3f266e5f86f1
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-06-29 22:58:02 +0300

    modify User type to include all user Data and Add Cred + Login And Register types

--- client/src/types/user.types.ts
+++ client/src/types/user.types.ts
@@ -1,7 +1,32 @@
+// type User = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   type: "student" | "teacher";
+//   // Add other user details as needed
+// };
+
 type User = {
   id: string;
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
-  type: "student" | "teacher";
-  // Add other user details as needed
-};
+  phoneNumber: string;
+  address: string;
+  birthDay: string;
+  type: 'student' | 'teacher';
+}
+
+type LoginUser = User & {
+  _id: string
+}
+
+type UserRegister = User & {
+  password: string;
+  confirm_password: string;
+}
+
+type UserCredentials = {
+  email: string;
+  password: string;
+}
