206f6566d2ca5e1c0a8608cce3a205c7537444ba
Yair Avivi <yavivi441@gmail.com>
2024-07-23 09:36:49 +0300

    use display cards component

--- client/src/pages/student-personal-area.tsx
+++ client/src/pages/student-personal-area.tsx
@@ -1,6 +1,9 @@
 import { useLessonsDetailsByUser } from "../hooks/lessons/useLessonsDetailsByUser";
 import { useUser } from "../contexts/user-context";
 import Loader from "../components/common/loader";
+import DisplayCards from "../components/common/display-cards/display-cards";
+import { useMediaQuery } from "@mui/material";
+import LessonCard from "../components/lessons/lesson-card/lesson-card";
 
 const StudentPersonalArea = () => {
   const { userDetails } = useUser();
@@ -9,14 +12,28 @@ const StudentPersonalArea = () => {
     isLoading,
     isError,
   } = useLessonsDetailsByUser(userDetails!.id);
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
+  const viewMode = isSmallScreen ? "list" : "grid";
+
+  const renderStudentCard = (lesson: LessonDetails) => (
+    <LessonCard lessonDetails={lesson} />
+  );
 
   return (
     <div>
       <div style={{ marginBottom: "8rem" }}>אזור אישי לתלמיד</div>
 
       {lessons ? (
-        <LessonsList lessons={lessons} />
+        <DisplayCards
+          items={lessons}
+          renderCard={renderStudentCard}
+          viewMode={viewMode}
+          xs={12}
+          sm={6}
+          md={3}
+        />
       ) : (
+        // <LessonsList lessons={lessons} />
         <p>
           {isLoading ? (
             <Loader message="טוען שיעורים" />
