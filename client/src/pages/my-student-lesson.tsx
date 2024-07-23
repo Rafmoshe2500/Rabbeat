469d413395969c8178502275f33316c3c8611e55
Yair Avivi <yavivi441@gmail.com>
2024-07-23 09:35:46 +0300

    add 2 pages and routes for them

---
+++ client/src/pages/my-student-lesson.tsx
@@ -0,0 +1,46 @@
+import { useLocation } from "react-router-dom";
+import { useMemo } from "react";
+import { useLessonsById } from "../hooks/lessons/useLessonById";
+import Loader from "../components/common/loader";
+import SelfTesting from "../components/self-testing/self-testing";
+import Chat from "../components/chat/chat";
+import TabsWrapper from "../components/common/tabs-wrapper/tabs-wrapper";
+
+const MyStudentLesson = () => {
+  const location = useLocation();
+  const lessonDetails: LessonDetails = location.state?.lessonDetails;
+  const { data: lesson, isLoading } = useLessonsById(lessonDetails.id!);
+
+  const lessonForView = useMemo(
+    () =>
+      ({
+        ...(lesson || {}),
+        ...(lessonDetails || {}),
+      } as Lesson),
+    [lesson, lessonDetails]
+  );
+
+  const tabs = [
+    {
+      name: "בחינה עצמית",
+      component: <SelfTesting lesson={lessonForView} />,
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {isLoading ? <Loader /> : <TabsWrapper tabs={tabs} />}
+
+      <Chat chatId={lessonDetails.chatId!} />
+    </div>
+  );
+};
+
+export default MyStudentLesson;
---
+++ client/src/pages/my-students-lessons.tsx
@@ -0,0 +1,54 @@
+import { useUser } from "../contexts/user-context";
+import Loader from "../components/common/loader";
+import DisplayCards from "../components/common/display-cards/display-cards";
+import { useMediaQuery } from "@mui/material";
+import LessonCard from "../components/lessons/lesson-card/lesson-card";
+import { useLocation } from "react-router-dom";
+import { useStudentLessonsByTeacher } from "../hooks/lessons/useStudentLessonsByTeacher";
+
+const MyStudentLessons = () => {
+  const location = useLocation();
+  const studentId: string = location.state?.id;
+
+  console.log({ studentId });
+
+  const { userDetails } = useUser();
+  const {
+    data: lessons,
+    isLoading,
+    isError,
+  } = useStudentLessonsByTeacher(userDetails!.id, studentId);
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
+  const viewMode = isSmallScreen ? "list" : "grid";
+
+  const renderStudentCard = (lesson: LessonDetails) => (
+    <LessonCard lessonDetails={lesson} />
+  );
+
+  return (
+    <div>
+      <div style={{ marginBottom: "8rem" }}>תלמיד חגם </div>
+
+      {lessons ? (
+        <DisplayCards
+          items={lessons}
+          renderCard={renderStudentCard}
+          viewMode={viewMode}
+          xs={12}
+          sm={6}
+          md={3}
+        />
+      ) : (
+        <p>
+          {isLoading ? (
+            <Loader message="טוען שיעורים" />
+          ) : (
+            "אין לך שיעורים כרגע"
+          )}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default MyStudentLessons;
