becd894d3403b2eb732a07e7240db4de3d9cbd8f
Yair Avivi <yavivi441@gmail.com>
2024-07-23 09:36:20 +0300

    add on click for the card

--- client/src/components/students-dashboard/student-card.tsx
+++ client/src/components/students-dashboard/student-card.tsx
@@ -1,44 +1,60 @@
-import React from 'react';
-import { Paper, Typography } from '@mui/material';
-import { styled } from '@mui/system';
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { useNavigate } from "react-router-dom";
 
 type StyledPaperProps = {
-  $viewMode: 'grid' | 'list';
+  $viewMode: "grid" | "list";
 };
 
 const StyledPaper = styled(Paper, {
-  shouldForwardProp: (prop) => prop !== '$viewMode',
+  shouldForwardProp: (prop) => prop !== "$viewMode",
 })<StyledPaperProps>(({ $viewMode }) => ({
-  padding: '16px',
-  textAlign: $viewMode === 'grid' ? 'center' : 'left',
-  color: '#666666',
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: $viewMode === 'grid' ? 'center' : 'flex-start',
-  alignItems: $viewMode === 'grid' ? 'center' : 'flex-start',
-  borderRadius: $viewMode === 'grid' ? 16 : 0,
-  transition: 'all 0.3s',
-  '&:hover': {
-    transform: $viewMode === 'grid' ? 'scale(1.05)' : 'none',
-    boxShadow: $viewMode === 'grid' ? '0px 6px 12px rgba(0, 0, 0, 0.15)' : 'none',
+  padding: "16px",
+  textAlign: $viewMode === "grid" ? "center" : "left",
+  color: "#666666",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: $viewMode === "grid" ? "center" : "flex-start",
+  alignItems: $viewMode === "grid" ? "center" : "flex-start",
+  borderRadius: $viewMode === "grid" ? 16 : 0,
+  transition: "all 0.3s",
+  "&:hover": {
+    transform: $viewMode === "grid" ? "scale(1.05)" : "none",
+    boxShadow:
+      $viewMode === "grid" ? "0px 6px 12px rgba(0, 0, 0, 0.15)" : "none",
   },
 }));
 
 interface StudentCardProps {
   student: Student;
-  viewMode: 'grid' | 'list';
+  viewMode: "grid" | "list";
 }
 
-const StudentCard: React.FC<StudentCardProps> = ({ student, viewMode }) => (
-  <StyledPaper elevation={viewMode === 'grid' ? 3 : 0} $viewMode={viewMode}>
-    <Typography variant={viewMode === 'grid' ? 'h6' : 'body1'} gutterBottom>
-      {`${student.firstName} ${student.lastName}`}
-    </Typography>
-    <Typography variant={viewMode === 'grid' ? 'body1' : 'body2'}>
-      {student.phoneNumber}
-    </Typography>
-  </StyledPaper>
-);
+const StudentCard: React.FC<StudentCardProps> = ({ student, viewMode }) => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    const route = `/my-students/${student.firstName}-${student.lastName}`;
+
+    navigate(route, { state: { id: student.id } });
+  };
+
+  return (
+    <StyledPaper
+      onClick={onClick}
+      elevation={viewMode === "grid" ? 3 : 0}
+      $viewMode={viewMode}
+    >
+      <Typography variant={viewMode === "grid" ? "h6" : "body1"} gutterBottom>
+        {`${student.firstName} ${student.lastName}`}
+      </Typography>
+      <Typography variant={viewMode === "grid" ? "body1" : "body2"}>
+        {student.phoneNumber}
+      </Typography>
+    </StyledPaper>
+  );
+};
 
 export default StudentCard;
