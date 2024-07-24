f8ce005c1543b06540847c7866feb3e42f25f1ca
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-07-25 00:07:26 +0300

    set https configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
+  },
+  server: {
+    https: {
+        key: fs.readFileSync('/home/cs128/ssc/key.pem'),
+        cert: fs.readFileSync('/home/cs128/ssc/cert.pem')
+    },
+    host: '0.0.0.0',
+    port: 443
   }
 })
 
