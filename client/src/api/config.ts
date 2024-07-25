35c67cfacd5b2a0ebc6fedf6e93bf4f6cd42dfd6
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-07-26 01:51:30 +0300

    last chance

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://localhost/api",
+  baseURL: "https://193.106.55.125/api",
   headers: {
     "Content-Type": "application/json",
   },
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -11,10 +9,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://localhost',
+        target: 'http://193.106.55.125:3000',
         changeOrigin: true,
       }
     }
   },
 })
-
