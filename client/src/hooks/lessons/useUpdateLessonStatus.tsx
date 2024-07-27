42fd73eee78812f815f08f2101b1f086f9e3e55e
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-07-28 00:15:41 +0300

    remove queryClient

--- client/src/hooks/lessons/useUpdateLessonStatus.tsx
+++ client/src/hooks/lessons/useUpdateLessonStatus.tsx
@@ -1,8 +1,7 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { updateLessonStatus } from "../../api/endpoints/lesson";
 
 export const useUpdateLessonStatus = () => {
-  const queryClient = useQueryClient();
 
   return useMutation<
     Boolean,
