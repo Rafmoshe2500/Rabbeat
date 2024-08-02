f28e5f0ac366be0935e6ab261cf37ce1a4d4cf99
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-08-02 15:17:49 +0300

    rollback and get out to new branch

--- client/src/components/students-dashboard/student-card.tsx
+++ client/src/components/students-dashboard/student-card.tsx
@@ -1,141 +1,201 @@
-import React from "react";
+import React, { useState } from "react";
+import { Paper, Typography, Menu, MenuItem, Tooltip } from "@mui/material";
+import { styled } from "@mui/system";
 import { useNavigate } from "react-router-dom";
-import {
-  Box,
-  Card,
-  CardActionArea,
-  CardContent,
-  Typography,
-  Chip,
-  Badge,
-  Tooltip,
-} from "@mui/material";
+import RTLDatePicker from "../common/rtl-inputs/rtl-date-picker";
+import dayjs from 'dayjs';
+import DialogComponent from "../common/dialog";
+import LED from '../common/led';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EventIcon from '@mui/icons-material/Event';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 
-type StudentCardProps = {
-  student: Partial<Student>;
-};
+const StyledPaper = styled(Paper, {
+  shouldForwardProp: (prop) => prop !== "$viewMode" && prop !== "$isExpired",
+})<{ $viewMode: "grid" | "list"; $isExpired: boolean }>(({ theme, $isExpired, $viewMode }) => ({
+  padding: theme.spacing(3),
+  margin: theme.spacing(1),
+  textAlign: "center",
+  color: $isExpired ? theme.palette.text.secondary : theme.palette.text.primary,
+  width: '100%',
+  maxWidth: $viewMode === "grid" ? "350px" : "100%",
+  height: $viewMode === "grid" ? "100%" : "auto",
+  display: "flex",
+  flexDirection: $viewMode === "grid" ? "column" : "row",
+  justifyContent: "space-between",
+  alignItems: $viewMode === "grid" ? "center" : "flex-start",
+  borderRadius: theme.shape.borderRadius * 2,
+  transition: "all 0.3s",
+  background: $isExpired
+    ? theme.palette.action.disabledBackground
+    : theme.palette.background.paper,
+  boxShadow: $isExpired
+    ? 'inset 5px 5px 10px rgba(0, 0, 0, 0.1), inset -5px -5px 10px rgba(255, 255, 255, 0.5)'
+    : '5px 5px 10px rgba(0, 0, 0, 0.1), -5px -5px 10px rgba(255, 255, 255, 0.5)',
+  position: "relative",
+  overflow: "hidden",
+  "&:hover": {
+    transform: "translateY(-5px)",
+    boxShadow: $isExpired
+      ? 'inset 7px 7px 14px rgba(0, 0, 0, 0.1), inset -7px -7px 14px rgba(255, 255, 255, 0.5)'
+      : '7px 7px 14px rgba(0, 0, 0, 0.1), -7px -7px 14px rgba(255, 255, 255, 0.5)',
+  },
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: "column",
+    alignItems: "center",
+    padding: theme.spacing(2),
+  },
+}));
 
-const StudentCard = ({ student }: StudentCardProps) => {
+const InfoContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: theme.spacing(1.5),
+  marginTop: theme.spacing(2),
+  [theme.breakpoints.down('sm')]: {
+    alignItems: 'center',
+  },
+}));
+
+const InfoItem = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.spacing(1),
+}));
+
+
+interface StudentCardProps {
+  student: Student;
+  viewMode: "grid" | "list";
+  onUpdateExpiredDate: (studentId: string, newExpiredDate: string) => void;
+}
+
+const StudentCard: React.FC<StudentCardProps> = ({ student, viewMode, onUpdateExpiredDate }) => {
   const navigate = useNavigate();
-  const {
-    id,
-    firstName,
-    lastName,
-    phoneNumber,
-    expired_date,
-    updated,
-  } = student;
+  const [contextMenu, setContextMenu] = useState<{ mouseX: number; mouseY: number } | null>(null);
+  const [openDatePicker, setOpenDatePicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(dayjs(student.expired_date));
 
-  const isExpired = new Date(expired_date!) < new Date();
-  const daysLeft = calculateDaysLeft(expired_date!);
-
-  const onClick = () => {
-    const route = `/my-students/${firstName}-${lastName}`;
-    navigate(route, { state: { id } });
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    const route = `/my-students/${student.firstName}-${student.lastName}`;
+    navigate(route, { state: { id: student.id } });
   };
 
-  const getBorderColor = () => {
-    if (isExpired) return "#9E9E9E";
-    if (updated) return "#FFB74D";
-    return "#4CAF50";
+  const handleContextMenu = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setContextMenu(
+      contextMenu === null
+        ? {
+            mouseX: event.clientX - 2,
+            mouseY: event.clientY - 4,
+          }
+        : null,
+    );
   };
 
-  const getStatusColor = () => {
-    if (isExpired) return "default";
-    if (updated) return "warning";
-    return "success";
+  const handleCloseMenu = () => {
+    setContextMenu(null);
   };
 
-  const getStatusText = () => {
-    if (isExpired) return "הסתיים";
-    if (updated) return "קיים עדכון";
-    return "פעיל";
+  const handleCloseDatePicker = () => {
+    setOpenDatePicker(false);
   };
+
+  const handleDateChange = (newDate: dayjs.Dayjs | null) => {
+    setSelectedDate(newDate);
+  };
+
+  const handleConfirmUpdateDate = () => {
+    if (selectedDate) {
+      onUpdateExpiredDate(student.id, selectedDate.toISOString());
+    }
+    setOpenDatePicker(false);
+  };
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
+  };
+
+  const calculateDaysLeft = (expiredDate: string) => {
+    const today = new Date();
+    const expirationDate = new Date(expiredDate);
+    const timeDiff = expirationDate.getTime() - today.getTime();
+    return Math.ceil(timeDiff / (1000 * 3600 * 24));
+  };
+
+  const isExpired = new Date(student.expired_date) < new Date();
+  const daysLeft = calculateDaysLeft(student.expired_date);
 
   return (
-    <Box sx={{ minWidth: 275, maxWidth: 345, margin: "1rem" }}>
-      <Card
-        variant="outlined"
-        sx={{
-          position: "relative",
-          border: `1px solid ${getBorderColor()}`,
-          transition: "0.3s",
-          "&:hover": {
-            boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)",
-          },
-        }}
+    <>
+      <StyledPaper
+        onClick={handleClick}
+        onContextMenu={handleContextMenu}
+        elevation={0}
+        $viewMode={viewMode}
+        $isExpired={isExpired}
       >
-        <CardActionArea onClick={onClick}>
-          <CardContent>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Typography variant="h5" component="div" gutterBottom>
-                {`${firstName} ${lastName}`}
-              </Typography>
-              <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                {updated && (
-                  <Tooltip title="קיים עדכון" arrow>
-                    <Badge color="primary" variant="dot">
-                      <NotificationsActiveIcon />
-                    </Badge>
-                  </Tooltip>
-                )}
-              </Box>
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: 'flex-start', mt: 2 }}>
-              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 2, marginLeft: '10px' }}>
-                <PhoneIcon fontSize="small" sx={{ mb: 1 }} />
-                <EventIcon fontSize="small" sx={{ mb: 1 }} />
-                <AccessTimeIcon fontSize="small" color={isExpired ? "error" : "inherit"} />
-              </Box>
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Typography variant="body2" sx={{ mb: 1 }}>{phoneNumber}</Typography>
-                <Typography variant="body2" sx={{ mb: 1 }}>{formatDate(expired_date!)}</Typography>
-                <Typography variant="body2" color={isExpired ? "error" : "inherit"}>
-                  {isExpired ? "הסתיים" : `${daysLeft} ימים לסיום`}
-                </Typography>
-              </Box>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                mt: 2,
-              }}
-            >
-              <Chip
-                label={getStatusText()}
-                size="small"
-                color={getStatusColor()}
-              />
-            </Box>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Box>
+        <Tooltip title={isExpired ? 'לא בשימוש' : (student.updated ? 'קיים עדכון' : 'אין עדכונים')}>
+          <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+            <LED status={isExpired ? 'off' : (student.updated ? 'half' : 'ok')} />
+          </div>
+        </Tooltip>        
+        <Typography variant="h4" gutterBottom>
+          {`${student.firstName} ${student.lastName}`}
+        </Typography>
+        
+        <InfoContainer>
+          <InfoItem>
+            <PhoneIcon fontSize="medium" color="primary" />
+            <Typography variant="body1">{student.phoneNumber}</Typography>
+          </InfoItem>
+          <InfoItem>
+            <AccessTimeIcon fontSize="medium" color={isExpired ? "error" : "primary"} />
+            <Typography variant="body1" color={isExpired ? "error" : "#616161"}>
+              {isExpired 
+                ? "הסתיים" 
+                : `${daysLeft} ימים לסיום`}
+            </Typography>
+          </InfoItem>
+          <InfoItem>
+            <EventIcon fontSize="medium" color="primary" />
+            <Typography variant="body1">
+              {formatDate(student.expired_date)}
+            </Typography>
+          </InfoItem>
+        </InfoContainer>
+      </StyledPaper>
+      <Menu
+        open={contextMenu !== null}
+        onClose={handleCloseMenu}
+        dir="rtl"
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu !== null
+            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+            : undefined
+        }
+      >
+        <MenuItem onClick={() => setOpenDatePicker(true)}>עדכן תאריך סיום.</MenuItem>
+      </Menu>
+      
+      <DialogComponent 
+        open={openDatePicker}
+        title="עדכן תאריך."
+        onClose={handleCloseDatePicker}
+        onConfirm={handleConfirmUpdateDate}
+      >
+        <RTLDatePicker
+          label="תאריך סיום חדש"
+          value={dayjs(student.expired_date)}
+          onChange={handleDateChange}
+        />
+      </DialogComponent>
+    </>
   );
-};
-
-const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
-};
-
-const calculateDaysLeft = (expiredDate: string) => {
-  const today = new Date();
-  const expirationDate = new Date(expiredDate);
-  const timeDiff = expirationDate.getTime() - today.getTime();
-  return Math.ceil(timeDiff / (1000 * 3600 * 24));
 };
 
 export default StudentCard;
