1976a7e066530c759fd29f8aa58e4d2f10c80887
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-08-11 22:23:10 +0300

    roll back

--- client/src/components/audio-recorder/audio-recorder.tsx
+++ client/src/components/audio-recorder/audio-recorder.tsx
@@ -96,7 +96,7 @@ const AudioRecorder: React.FC<AudioRecorderProps> = ({
     if (transLength > currTransLength) {
       setTimestamps((prevTimestamps) => [
         ...prevTimestamps,
-        (Date.now() - startTime) / 1000,
+        (Date.now() - startTime - 300) / 1000,
       ]);
     } else if (transLength < currTransLength) {
       setTimestamps((prevTimestamps) => prevTimestamps.slice(0, -1));
