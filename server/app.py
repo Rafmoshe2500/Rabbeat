06f9f34604473f07b12012f1dfdaeeca4ed8df00
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-08-11 23:14:58 +0300

    set 100MB request

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.responses import JSONResponse
@@ -12,15 +12,25 @@ from tools.consts import MB
 app = FastAPI(title='Rabbeat')
 
 app.add_middleware(
-    TrustedHostMiddleware,
     CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    max_body_size=100 * MB  # 100 MB
-
 )
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+
+
+@app.middleware("http")
+async def increase_request_body_size(request: Request, call_next):
+    BODY_SIZE_LIMIT = 100 * MB  # 100 MB
+    if request.method == 'POST':
+        request._body_size_limit = BODY_SIZE_LIMIT
+    response = await call_next(request)
+    return response
+
+
 app.include_router(torah_router)
 app.include_router(lessons.router, prefix="/api")
 app.include_router(user_lessons.router, prefix="/api")
@@ -41,4 +51,5 @@ async def validation_exception_handler(_, exc):
     )
 
 
-uvicorn.run(app, host="0.0.0.0", port=3000, limit_max_requests=100 * MB)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
