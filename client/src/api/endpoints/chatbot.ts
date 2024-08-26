5d17f795dfadafe37ba8ccebf883e78b9657916c
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-08-26 23:57:37 +0300

    add header woth jwt

--- client/src/api/endpoints/chatbot.ts
+++ client/src/api/endpoints/chatbot.ts
@@ -1,11 +1,20 @@
+import { getToken } from '../../utils/jwt-cookies';
 import apiClient from '../config';
 
-export const sendMessageToChatbot = async (message: ChatBotMessage): Promise<ChatBotMessage> => {
-    try {
-      const response = await apiClient.post<ChatBotMessage>('/chat/', message);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  };
+export const sendMessageToChatbot = async (
+  message: ChatBotMessage
+): Promise<ChatBotMessage> => {
+  try {
+    const token = getToken();
+    const response = await apiClient.post<ChatBotMessage>('/chat/', message, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
   
